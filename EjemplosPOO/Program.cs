17a07f54d61e6984dcaa38a1704a0a4312dfd579
HectorOC-e <osoriohector89@gmail.com>
2022-12-27 21:39:27 -0600

    Agregar archivos de proyecto.

---
+++ EjemplosPOO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace EjemplosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Circulo miCirculo; // Creación de objetoo de tipo Círculo. Variable/Objeto de tipo círculo
+            //miCirculo = new Circulo();
+            //Console.WriteLine($"Area: {miCirculo.CalcularArea(5)}");
+
+            //Circulo miCirculo2 = new Circulo();
+
+            //Console.WriteLine($"Area: {miCirculo.CalcularArea(9)}");
+
+            ConversorEuroDolar obj= new ConversorEuroDolar();
+
+            obj.euro = ;
+
+            Console.WriteLine($"Son {obj.Convierte(50)} dolares");
+
+        }
+    }
+
+    class Circulo
+    {
+        private const double pi = 3.1416;
+
+        public double CalcularArea(int radio) => pi * radio * radio;
+    }
+
+    class ConversorEuroDolar
+    {
+        private double euro = 1.253;
+
+        public double Convierte(double cantidad) => euro * cantidad;
+
+        public void CambioValorEuro(double nuevoValor)
+        {
+            if (nuevoValor >= 0) euro = nuevoValor;
+        }
+    }
+}
